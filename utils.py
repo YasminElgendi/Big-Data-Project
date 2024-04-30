aa326f632984fa55168af1594a97646a97be47e6
Yasmin Elgendi <yasminelgendi@gmail.com>
2024-04-30 19:31:53 +0300

    Update utils

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ from pyspark.sql import SparkSession
 
 def load_data(file_path):
     spark = SparkSession.builder.appName("Get Data").getOrCreate()
-    data = spark.read.format('csv').option('header', 'true').option('inferSchema', 'true').load(file_path)
-    return data
+    df = spark.read.format('csv').option('header', 'true').option('inferSchema', 'true').load(file_path)
+    return df
 
-def save_data(data, file_path):
-    data.toPandas().to_csv(file_path, index=False)
+def save_data(df, file_path):
+    df.toPandas().to_csv(file_path, index=False)
